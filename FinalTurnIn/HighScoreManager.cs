ccbef8705e883ae385559a66488585c8abfc25ff
Frooddie <143598017+Frooddie@users.noreply.github.com>
2025-02-02 23:57:58 +0100

    Initial commit

---
+++ FinalTurnIn/HighScoreManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalTurnIn
+{
+    public class HighScoreManager : IHighScoreStorage
+    {
+        private List<int> highScores = new List<int>();
+
+        public void SaveHighScore(int score)
+        {
+            highScores.Add(score);
+            highScores = highScores.OrderBy(s => s).Take(5).ToList();  // Håller bara topp 5
+            DisplayHighScores();
+        }
+
+        public void DisplayHighScores()
+        {
+            Console.WriteLine("\nTop 5 High Scores:");
+            foreach (var score in highScores)
+            {
+                Console.WriteLine(score);
+            }
+        }
+    }
+
+}
---
+++ FinalTurnIn/UserInputValidator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace FinalTurnIn
+{
+
+    public class UserInputValidator : IUserInputValidator
+    {
+        public bool Validate(string input, out int userGuess)
+        {
+            userGuess = 0;
+            var validator = new NumberValidator();
+            var validationResult = validator.Validate(input);
+            if (validationResult.IsValid)
+            {
+                userGuess = int.Parse(input);
+                return userGuess >= 1 && userGuess <= 100;
+            }
+            return false;
+        }
+    }
+
+    public class NumberValidator : AbstractValidator<string>
+    {
+        public NumberValidator()
+        {
+            RuleFor(x => x).Matches(@"^\d+$").WithMessage("Input måste vara ett nummer.");
+        }
+    }
+
+    public interface IUserInputValidator
+    {
+        bool Validate(string input, out int userGuess);
+    }
+
+}
